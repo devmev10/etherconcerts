43a9a3502f3e7898281e9341c43c8e64f0b9f7e6
Ravi Tokas <ravitokas90@gmail.com>
2023-08-02 19:42:03 +0530

    test: update TokenMaster.js

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,20 +1,23 @@
 const { expect } = require("chai");
 
+const NAME = "TokenMaster";
+const SYMBOL = "TM";
+
 describe("TokenMaster", () => {
   let tokenMaster;
 
   beforeEach(async () => {
     const TokenMaster = await ethers.getContractFactory("TokenMaster");
-    tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
+    tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
   });
 
   describe("Deployment", () => {
     it("Sets the name", async () => {
-      expect(await tokenMaster.name()).to.equal("TokenMaster");
+      expect(await tokenMaster.name()).to.equal(NAME);
     });
 
     it("Sets the symbol", async () => {
-      expect(await tokenMaster.symbol()).to.equal("TM");
+      expect(await tokenMaster.symbol()).to.equal(SYMBOL);
     });
   });
 });
