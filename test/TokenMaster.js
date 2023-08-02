0aada3232feaf11a3616e93f053e7ae59eda976a
devmev10 <ravitokas90@gmail.com>
2023-08-02 15:15:09 +0530

    test: update TokenMaster.js

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,3 +1,12 @@
 const { expect } = require("chai");
 
-describe("TokenMaster", () => {});
+describe("TokenMaster", () => {
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      const TokenMaster = await ethers.getContractFactory("TokenMaster");
+      let tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
+      let name = await tokenMaster.name();
+      expect(name).to.equal("TokenMaster");
+    });
+  });
+});
