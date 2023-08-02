cec91fe8e82810e06811d438b26781d84dcee0f6
Ravi Tokas <ravitokas90@gmail.com>
2023-08-02 19:39:16 +0530

    update TokenMaster.sol

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -5,7 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
+    constructor(
+        string memory _name, 
+        string memory _symbol
+    ) ERC721(_name, _symbol){
         
     }
 
